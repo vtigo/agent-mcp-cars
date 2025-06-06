54c6ec580596c0457e9db2c96f061aa04e65a676
tigo <rf.tigas@gmail.com>
2025-06-06 17:56:14 -0300

    feat(agent): add memory and some system prompt refactor

--- src/app/agent/agent_llm.py
+++ src/app/agent/agent_llm.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
 from langgraph.prebuilt import create_react_agent
 from app.agent.tools import query_cars_tool
+from langgraph.checkpoint.memory import InMemorySaver
 
 load_dotenv()
 
@@ -17,9 +18,9 @@ system_prompt = (
         "You are Mick, a helpful assistant specialized in searching a cars database.\n"
         "If the user’s question does not require any database lookup, respond with plain natural language only — no JSON or function call objects.\n\n"
         "When the user asks for specific types of cars (e.g. by brand, color, year), "
-        "you must extract filters and use the `query_cars_tool` to search the database. "
+        "you must extract filters and use the `query_cars_tool` to search the database.\n"
+        "When the user dont expecify features of the car immediatialy, guide him through a series of 2 or 3 questions to find out what filters to apply to the search.\n\n"
         "When the tool returns results, read the output and write a helpful response based on it.\n\n"
-
         "If the user's question is general (e.g. about engines, car types, or concepts), answer directly, without invoking any tool.\n\n"
 
         "=== Examples ===\n\n"
@@ -48,6 +49,25 @@ system_prompt = (
         "Final Answer: I found one black 4-door SUV with automatic transmission:\n"
         "- Toyota SW4 (2022), diesel, R$240000\n\n"
 
+        "User: I am looking for a car.\n"
+        "Thought: The user is looking for a car but has not specified any features. I will ask him 1 to 3 questions before calling the tool.\n"
+        "Answer 1: Sure! What category of car are you looking for?\n"
+        "User: I am looking for an SUV\n"
+        "Thought: The user is looking for a SUV model. I will try to obtain more information before calling the tool.\n"
+        "Answer 2: What are some important features that a car must have for you?\n"
+        "User: The car should be silver and it must be automatic\n"
+        "Thought: The user is looking for a black SUV car with automatic transmission. I will ask him if there is any more information that i should include in the filters.\n"
+        "Answer 3: Is there anything else that the car must include?\n"
+        "User: That is it.\n"
+        "Thought: The user seems satisfyed with the features presented. I will call the query_cars_tool.\n"
+        "Action: query_cars_tool.\n"
+        "Action Input: {\"category\": \"suv\", \"color\": \"silver\", \"transmission\": \"automatic\"}\n"
+        "Observation: Found 1 car:\n"
+        "- BMW X4 (2019), silver, flex, automatic, 4 doors, R$320000\n"
+        "Final Answer: I found one silver SUV with automatic transmission:\n"
+        "- BMW X4 (2019), silver, automatic transmission for R$320000\n\n"
+
+
         "=== End of Examples ===\n\n"
 
         "Always reason step-by-step and clearly decide whether to use the tool.\n"
@@ -55,18 +75,21 @@ system_prompt = (
         "how many matches were found, and list every car."
         )
 
+checkpointer = InMemorySaver()
+
 agent = create_react_agent(
         model=model,
         tools=[query_cars_tool],
-        prompt=system_prompt
+        prompt=system_prompt,
+        checkpointer=checkpointer
         )
 
-def send_prompt(prompt: str) -> str:
+def send_prompt(prompt: str, thread_id: str = "default") -> str:
     """
     Sends a prompt to the initialized agent and returns the final AI response.
     """
     try:
-        result = agent.invoke({"messages": [{"role": "user", "content": prompt}]})
+        result = agent.invoke({"messages": [{"role": "user", "content": prompt}]}, {"configurable": {"thread_id": thread_id}})
         messages = result.get("messages", [])
 
         # Reverse iterate through message objects and extract valid AI responses
