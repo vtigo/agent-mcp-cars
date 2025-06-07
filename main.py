9c137f4855fa995a2c3ce171fad9bc6e87bc637a
tigo <rf.tigas@gmail.com>
2025-06-07 02:19:27 -0300

    feat(cli): add main entrypoint at root for PYTHONPATH

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+SRC_DIR = os.path.join(os.path.dirname(__file__), "src")
+if SRC_DIR not in sys.path:
+    sys.path.insert(0, SRC_DIR)
+
 from app.runner import main
 
 if __name__ == "__main__":
